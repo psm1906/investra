3c5f4700b5d078e9209df709802008d7b51e8216
anu_boyapati <anu.d.boyapati@gmail.com>
2025-04-05 14:27:16 -0500

    resetting

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,9 @@
-from flask import Flask, jsonify
-from flask_cors import CORS
-
+from flask import Flask
 app = Flask(__name__)
-CORS(app)
 
-@app.route('/api/transactions')
-def transactions():
-    placeholder_data = [
-        {"id": 1, "description": "Test Transaction 1", "amount": 100},
-        {"id": 2, "description": "Test Transaction 2", "amount": 200},
-    ]
-    return jsonify(placeholder_data)
+@app.route('/')
+def hello():
+    return 'Hello, World!'
 
 if __name__ == '__main__':
     app.run(debug=True)
